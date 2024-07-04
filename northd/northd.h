1febd2ed0b21871230e66839f88851d32126c581
Martin Kalcok <martin.kalcok@canonical.com>
2024-07-04 19:02:29 +0200

    northd: Routing protocol port redirection.
    This change adds two new LRP options:
     * routing-protocol-redirect
     * routing-protocols
    
    That allow redirection of a routing protocol traffic to
    an Logical Switch Port. This enables external routing daemons
    to listen on an interface bound to an LSP and effectively act
    as if they were listening on (and speaking from) LRP's IP address.
    
    Option 'routing-protocols' takes comma-separated list of routing
    protocols whose traffic should be redirected. Currently supported
    are BGP (tcp 179) and BFD (udp 3784).
    
    Option 'routing-protocol-redirect' expects a string with an LSP
    name.
    
    When both of these options are set, any traffic entering LS
    that's destined for LRP's IP addresses (including IPv6 LLA) and
    routing protocol's port number, is redirected to the LSP specified
    in the 'routing-protocol-redirect' value.
    
    NOTE: this feature is experimental and may be subject to
    removal/change in the future.
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

--- northd/northd.h
+++ northd/northd.h
@@ -89,6 +89,13 @@ ods_size(const struct ovn_datapaths *datapaths)
 
 bool od_has_lb_vip(const struct ovn_datapath *od);
 
+/* List of routing and routing-related protocols which
+ * OVN is capable of redirecting from LRP to specific LSP. */
+enum redirected_routing_protcol_flag_type {
+    REDIRECT_BGP = (1 << 0),
+    REDIRECT_BFD = (1 << 1),
+};
+
 struct tracked_ovn_ports {
     /* tracked created ports.
      * hmapx node data is 'struct ovn_port *' */
